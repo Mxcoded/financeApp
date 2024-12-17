46023604a5e1a3498a7db99d30036d438f2ee3eb
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2024-12-17 10:02:34 +0100

    added Button that links to create

--- resources/views/file/index.blade.php
+++ resources/views/file/index.blade.php
@@ -9,22 +9,38 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h1>Uploaded Files</h1>
-                    <table>
+                    <!-- Header with Add Button -->
+                    <div class="flex justify-between items-center mb-6">
+                        <h1 class="text-lg font-semibold">Uploaded Files</h1>
+                        <a href="{{ route('files.create') }}" 
+                           class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <!-- Plus Icon -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+                            </svg>
+                            Add Upload
+                        </a>
+                    </div>
+
+                    <!-- Files Table -->
+                    <table class="min-w-full bg-white border border-gray-200">
                         <thead>
                             <tr>
-                                <th>#</th>
-                                <th>File Name</th>
-                                <th>Actions</th>
+                                <th class="py-2 px-4 border-b">#</th>
+                                <th class="py-2 px-4 border-b">File Name</th>
+                                <th class="py-2 px-4 border-b">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($files as $file)
                                 <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $file->file_name }}</td>
-                                    <td>
-                                        <a href="{{ route('files.download', $file->id) }}">Download</a>
+                                    <td class="py-2 px-4 border-b">{{ $loop->iteration }}</td>
+                                    <td class="py-2 px-4 border-b">{{ $file->file_name }}</td>
+                                    <td class="py-2 px-4 border-b">
+                                        <a href="{{ route('files.download', $file->id) }}" 
+                                           class="text-blue-500 hover:text-blue-700">
+                                            Download
+                                        </a>
                                     </td>
                                 </tr>
                             @endforeach
